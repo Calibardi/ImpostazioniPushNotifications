391f3f44a8b8c0c1eeb28d3e0a755813b33a7610
Lorenzo Ilardi <stillduke@gmail.com>
2020-10-12 16:20:31 +0200

    boh

---
+++ ImpostazioniPushNotifications/ListaReparti.swift
@@ -0,0 +1,112 @@
+//
+//  ListaReparti.swift
+//  ImpostazioniPushNotifications
+//
+//  Created by Lorenzo Ilardi on 12/10/2020.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var pushNotificationsSwitch: UISwitch!
+
+}
+
+class ListaReparti: UITableViewController {
+    
+    let reparti = ["Reparto surgelati", "Reparto frutta", "Reparto reparti","Reparto fai-da-te", "Reparto benessere", "Reparto igiene"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.separatorColor = UIColor.clear
+        tableView.rowHeight = 100.0
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return reparti.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RepartoCell", for: indexPath) as! CustomCell
+
+        // Configure the cell...
+        cell.nameLabel.text = reparti[indexPath.row]
+//        cell.backgroundColor = UIColor.white
+//        cell.layer.borderColor = UIColor.black.cgColor
+//        cell.layer.borderWidth = 1
+//        cell.layer.cornerRadius = 8
+//        cell.clipsToBounds = true
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Lista Reparti"
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+           return 44
+       }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ImpostazioniPushNotifications/Model/DataModel.swift
@@ -0,0 +1,16 @@
+//
+//  DataModel.swift
+//  ImpostazioniPushNotifications
+//
+//  Created by Lorenzo Ilardi on 12/10/2020.
+//
+
+import Foundation
+
+struct Negozi {
+    var name: String? = "nil"
+    var reparti: [String]
+}
+
+
+
